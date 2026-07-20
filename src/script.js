f931bf0dea44b192d729e7c3be4bd912aeaf6f0a
Mubby-K <mubaarakah123@gmail.com>
2026-07-20 13:31:33 +0000

    Added script.js and modified index.html

---
+++ src/script.js
@@ -0,0 +1,21 @@
+// taking input for the user
+let userName = prompt("Enter your name");
+// alert(userName);
+
+
+let age = prompt("Enter your age" );
+
+// alert(age);
+
+
+let email = prompt("Enter your email");
+// alert(email);
+
+// Create the message string using template literals
+let message = `Hello ${userName}, you are ${age} years old, and we'll keep in touch via your email: ${email} `;
+alert(message);
+
+
+
+
+
