3ef7c3771ea0e248267ce3251b4efeb0ad850034
Mubby-K <mubaarakah123@gmail.com>
2026-07-22 09:14:33 +0000

    Added Assignment 9

--- src/script.js
+++ src/script.js
@@ -15,6 +15,48 @@ let email = prompt("Enter your email");
 let message = `Hello ${userName}, you are ${age} years old, and we'll keep in touch via your email: ${email} `;
 alert(message);
 
+// Assignment 9
+let myName = "Mubaarakah";
+console.log(userName);
+
+let myAge = "26";
+console.log(26);
+
+// Adding numbers
+let x = 25;
+let y = 44;
+let addNumber = x + y;
+console.log(x + y);
+
+// Substracting Numbers
+let subNumbers = y - x;
+console.log(y - x);
+
+// Dividing Numbers
+let divNumbers = x / y;
+console.log(x / y);
+
+// Multiplying Numbers
+let productNumber = x * y;
+console.log(x * y);
+
+// finding Modal
+x = 15
+y = 4
+let remainder = x % y;
+console.log(x % y);
+
+// boolean value
+myName = true;
+console.log(true);
+
+// Setting Variable to a Number
+console.log(typeof(24));
+
+// Setting Variable to a string
+console.log(typeof("Mubaarakah"));
+
+
 
 
 
